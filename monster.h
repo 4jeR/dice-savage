688d1326e5091d3e1294b9fc960f43cf68d7a948
4jeR <bdlugosz98@gmail.com>
2019-07-27 13:29:28 +0200

    add files

---
+++ monster.h
@@ -0,0 +1,11 @@
+#ifndef MONSTER_H
+#define MONSTER_H
+
+
+class Monster
+{
+public:
+    Monster();
+};
+
+#endif // MONSTER_H
---
+++ statstracker.h
@@ -0,0 +1,11 @@
+#ifndef STATSTRACKER_H
+#define STATSTRACKER_H
+
+
+class StatsTracker
+{
+public:
+    StatsTracker();
+};
+
+#endif // STATSTRACKER_H
