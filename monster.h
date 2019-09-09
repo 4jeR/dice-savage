027f51e87d02bb1a2fdf0c8f208aabf851e91e72
4jeR <bdlugosz98@gmail.com>
2019-09-09 12:08:33 +0200

    code refactor in monster.h

--- monster.h
+++ monster.h
@@ -11,24 +11,18 @@ public:
     Monster(QString name="empty", QPixmap pixmap = QPixmap(), int lvl = 0, float dices = 0.0);
     Monster(const Monster& rhs);
 
-
     bool operator<(const Monster& rhs);
-
     Monster& operator=(const Monster& rhs);
 
     QString Name()const;
     QPixmap& PixMap();
-
+    bool& CanAttack();
     int Lvl()const;
     float Dices()const;
-    bool& CanAttack();
-
     int CalculateDamage()const;
-
 private:
     QString m_name;
     QPixmap m_pixmap;
-
     int m_lvl;
     float m_dices;
     bool m_canAttack;
