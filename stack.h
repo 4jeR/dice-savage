27ceb10fc7a280ff77e1bb55dc897d863fca2282
4jeR <bdlugosz98@gmail.com>
2019-09-09 12:10:36 +0200

    code refactor in stack.h

--- stack.h
+++ stack.h
@@ -7,21 +7,17 @@
 #include <QFile>
 #include <QMessageBox>
 #include <QTextStream>
-
 #include <string>
 #include <algorithm>
 #include <string>
 #include <utility>
-
 #include "monster.h"
 #include "options.h"
 
 class Stack
 {
 public:
-
     Stack(const std::vector<Monster>& bestiaryVec);
-
     std::vector<Monster>& GetStack();
 private:
     std::vector<Monster> m_stack;
