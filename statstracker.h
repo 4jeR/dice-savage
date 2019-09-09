91c1d3c86cbbdc81c9322f6db8038793f408e880
4jeR <bdlugosz98@gmail.com>
2019-09-09 12:11:10 +0200

    code refactor in statstracker.h

--- statstracker.h
+++ statstracker.h
@@ -1,17 +1,13 @@
 #ifndef STATSTRACKER_H
 #define STATSTRACKER_H
 
-
 struct StatsTracker
 {
 public:
     StatsTracker() = delete;
-
     static int s_seed;
     static int s_stackSize;
     static int s_turn;
 };
-
-
 
 #endif // STATSTRACKER_H
